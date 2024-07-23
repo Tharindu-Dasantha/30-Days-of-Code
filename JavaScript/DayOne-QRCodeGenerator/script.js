814f3c65895282afd575aecc7c36caa862ab3fb8
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-07-23 18:29:56 +0530

    Created the format selector
    added the download button.
    need to add the download option soon

--- JavaScript/DayOne-QRCodeGenerator/script.js
+++ JavaScript/DayOne-QRCodeGenerator/script.js
@@ -3,12 +3,13 @@ let qrImage = document.getElementById('qrImage');
 let qrText = document.getElementById('qrText');
 let qrX = document.getElementById('qrX');
 let qrY = document.getElementById('qrY');
+let format = document.getElementById('qrFormat').value;
 
 function Generate() {
     if (qrText.value.length > 0) {
         // Generate QR code
         let size = qrX.value + "x" + qrY.value;
-        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size="+ size + "&data=" + qrText.value;
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size="+ size + "&format=" + format +"&data=" + qrText.value;
         imgBox.classList.add('show-img');
     } else {
         qrText.classList.add("error");
