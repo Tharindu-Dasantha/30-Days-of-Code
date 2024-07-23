7c523adf25fcc685f6eac7e9f336871a738702d7
Dasantha Edirisinghe <t.dasanthaedirisinghe@gmail.com>
2024-07-23 18:17:33 +0530

    Added the size option

--- JavaScript/DayOne-QRCodeGenerator/script.js
+++ JavaScript/DayOne-QRCodeGenerator/script.js
@@ -1,11 +1,13 @@
 let imgBox = document.getElementById('imgBox');
 let qrImage = document.getElementById('qrImage');
 let qrText = document.getElementById('qrText');
+let qrX = document.getElementById('qrX');
+let qrY = document.getElementById('qrY');
 
 function Generate() {
     if (qrText.value.length > 0) {
         // Generate QR code
-        let size = "150x150";
+        let size = qrX.value + "x" + qrY.value;
         qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size="+ size + "&data=" + qrText.value;
         imgBox.classList.add('show-img');
     } else {
